6ee8f57402a625f92f586e7c30e87d97c74e2936
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2024-07-09 11:51:19 -0300

    Fixed property name in test project

--- test/src/DiasUteis.cs
+++ test/src/DiasUteis.cs
@@ -18,8 +18,8 @@ namespace TestAPI
         // Assert
         this.output.WriteLine($"Valor referencia esperado: {dia.referencia}, Valor referencia calculado: {diasUteis.referencia}");
         Assert.Equal(dia.referencia, diasUteis.referencia);
-        this.output.WriteLine($"Valor identificador esperado: {dia.identificador}, Valor identificador calculado: {diasUteis.identificador}");
-        Assert.Equal(dia.identificador, diasUteis.identificador);
+        this.output.WriteLine($"Valor identificador esperado: {dia.id_dias_uteis}, Valor identificador calculado: {diasUteis.id_dias_uteis}");
+        Assert.Equal(dia.id_dias_uteis, diasUteis.id_dias_uteis);
         this.output.WriteLine($"Valor dias_uteis esperado: {dia.dias_uteis}, Valor dias_uteis calculado: {diasUteis.dias_uteis}");
         Assert.Equal(dia.dias_uteis, diasUteis.dias_uteis);
       }
