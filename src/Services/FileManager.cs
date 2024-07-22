c9da2e0bce8c12360d1e1771b937802241caf357
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2024-07-22 11:13:18 -0300

    Removed all database dependencies from the file parser

--- src/Services/FileManager.cs
+++ src/Services/FileManager.cs
@@ -4,11 +4,9 @@ namespace backend.Services;
 public class FileManager
 {
   private readonly String filename;
-  private readonly Database database;
   private readonly Stream stream;
-  public FileManager(Database database, Stream stream, String filename)
+  public FileManager(Stream stream, String filename)
   {
-    this.database = database;
     this.stream = stream;
     this.filename = filename;
   }
@@ -68,7 +66,7 @@ public class FileManager
         servico.recurso = values[0];
         servico.dia = Conversor.GetDateOnly(values[1]);
         servico.serial = Conversor.GetNumberMiddle(values[2]);
-        servico.id_servico_situacao = this.database.servico_situacao.Where(s => s.servico_situacao == values[3]).Single().id_servico_situacao;
+        servico.id_servico_situacao = values[3];
         servico.nome_do_cliente = values[4];
         servico.endereco_destino = values[5];
         servico.cidade_destino = values[6];
@@ -145,11 +143,8 @@ public class FileManager
         servico.abreviacao = Conversor.Abreviacao(servico.recurso);
         servico.identificador = Conversor.Identificador(servico.dia, servico.abreviacao);
         servico.timestamp = Conversor.GetDateTime(servico.dia, servico.hora_final);
-        var atividade = this.database.servico_tipo.Find(servico.tipo_de_nota_de_servico);
-        if(atividade != null) servico.id_atividade = this.database.atividade.Where(a => a.atividade == atividade.servico_tipo).Single().id_atividade;
-        var processo = this.database.processo_atividade.Find(servico.id_atividade);
-        if(processo != null) servico.id_processo = processo.id_processo;
-        if(servico.codigos_concaternados == "20.5") servico.id_servico_situacao = this.database.servico_situacao.Where(s => s.servico_situacao == "concluído").Single().id_servico_situacao;
+        if(servico.codigos_concaternados == "20.5") servico.id_servico_situacao = "concluído";
+        servico.Valoracao();
         servicos.Add(servico);
       }
     }
@@ -182,12 +177,10 @@ public class FileManager
       {
         var composicao = new Composicao();
 
-
         temp = worksheet.GetValue<string>(row, cabecalho["data"]);
         composicao.dia = Conversor.GetDateOnly(temp);
         if(composicao.dia == null)
           composicao.validacao.Add($"A data informada ({temp}) é inválida!");
-
 
         temp = worksheet.GetValue<string>(row, cabecalho["adesivo"]);
         composicao.adesivo = Conversor.GetNumberMiddle(temp);
@@ -204,15 +197,10 @@ public class FileManager
         if(composicao.recurso == null)
           composicao.validacao.Add($"O recurso informado ({temp}) é inválido!");
 
-        temp = worksheet.GetValue<string>(row, cabecalho["atividade"]);
-        temp = composicao.SanitizarAlavanca(temp);
-        temp = composicao.IsPesado(temp);
-        temp = composicao.IsNoturno(temp);
-        var atividade = this.database.atividade.Where(a => a.atividade == temp).SingleOrDefault();
-        if(atividade != null)
-          composicao.id_atividade = atividade.id_atividade;
-        else
-          composicao.validacao.Add($"A atividade informada ({temp}) é inválida!");
+        composicao.atividade = worksheet.GetValue<string>(row, cabecalho["atividade"]);
+        composicao.SanitizarAlavanca();
+        composicao.IsPesado();
+        composicao.IsNoturno();
 
         temp = worksheet.GetValue<string>(row, cabecalho["motorista"]);
         composicao.motorista = temp;
@@ -222,9 +210,6 @@ public class FileManager
         if(composicao.id_motorista == null)
           composicao.validacao.Add($"A matrícula 1 informada ({temp}) é inválida!");
 
-        if(!this.if_exist(composicao.id_motorista, composicao.motorista))
-          composicao.validacao.Add($"{composicao.id_motorista}: {composicao.motorista} não foi encontrado na lista ou nome não corresponde a matrícula!");
-
         temp = worksheet.GetValue<string>(row, cabecalho["ajudante"]);
         composicao.ajudante = temp;
 
@@ -233,52 +218,25 @@ public class FileManager
         if(composicao.id_ajudante == null)
           composicao.validacao.Add($"A matrícula 2 informada ({temp}) é inválida!");
 
-        if (!this.if_exist(composicao.id_ajudante, composicao.ajudante))
-          composicao.validacao.Add($"{composicao.id_ajudante}: {composicao.ajudante} não foi encontrado na lista ou nome não corresponde a matrícula!");
-
         temp = worksheet.GetValue<string>(row, cabecalho["telefone"]);
         composicao.telefone = Conversor.GetPhoneNumber(temp);
         if(composicao.telefone == null)
           composicao.validacao.Add($"O telefone informado ({temp}) é inválido!");
-        }
 
-        temp = worksheet.GetValue<string>(row, cabecalho["supervisor"]);
-        composicao.supervisor = temp;
+        composicao.supervisor = worksheet.GetValue<string>(row, cabecalho["supervisor"]);
 
         if (cabecalho.TryGetValue("id_supervisor", out Int32 coluna))
         {
           temp = worksheet.GetValue<string>(row, coluna);
-          if(Int32.TryParse(temp, out Int32 id_supervisor))
-          {
-            composicao.id_supervisor = id_supervisor;
-          }
-          else
-          {
+          composicao.id_supervisor = Conversor.GetNumberMiddle(temp);
+          if(composicao.id_supervisor == null)
             composicao.validacao.Add($"A matrícula sup. informada ({temp}) é inválida!");
-          }
-        }
-        else
-        {
-          var id_supervisor = this.if_exist(composicao.supervisor);
-          composicao.id_supervisor = id_supervisor;
-        }
-        if(!this.if_exist(composicao.id_supervisor, composicao.supervisor))
-          composicao.validacao.Add($"{composicao.id_supervisor}: {composicao.supervisor} não foi encontrado na lista ou nome não corresponde a matrícula!");
-
-        temp = worksheet.GetValue<string>(row, cabecalho["regional"]);
-        temp = composicao.SanitizarRegional(temp);
-        composicao.id_regional = this.database.regional.Where(r => r.regional == temp).Single().id_regional;
-
-        if (cabecalho.TryGetValue("controlador", out coluna))
-        {
-          temp = worksheet.GetValue<string>(row, coluna);
-          composicao.controlador = temp;
-          var id_controlador = this.if_exist(composicao.controlador);
-          composicao.id_controlador = id_controlador;
-          if(composicao.id_controlador == 0)
-            composicao.validacao.Add($"{composicao.id_controlador}: {composicao.controlador} não foi encontrado na lista ou nome não corresponde a matrícula!");
         }
 
+        composicao.regional = worksheet.GetValue<string>(row, cabecalho["regional"]);
+        composicao.SanitizarRegional();
+
+        composicao.controlador = cabecalho.TryGetValue("controlador", out coluna) ? worksheet.GetValue<string>(row, coluna) : null;
         composicao.justificada = cabecalho.TryGetValue("justificada", out coluna) ? worksheet.GetValue<string>(row, coluna) : null;
         composicao.situacao = cabecalho.TryGetValue("situacao", out coluna) ? worksheet.GetValue<string>(row, coluna) : null;
         composicao.tecnico = cabecalho.TryGetValue("tecnico", out coluna) ? worksheet.GetValue<string>(row, coluna) : null;
@@ -289,42 +247,10 @@ public class FileManager
           composicao.identificador = Conversor.Identificador(composicao.dia, composicao.abreviacao);
         }
 
-        // composicao.id_processo = this.is_setor(composicao.atividade);
-
         composicoes.Add(composicao);
       }
     }
     return composicoes;
-  }
-  private bool if_exist(Int32? matricula, String? nome_colaborador)
-  {
-    if(matricula == null || nome_colaborador == null) return false;
-    var func = this.database.funcionario.Find(matricula);
-    if(func == null) return false;
-    if(!(func.nome_colaborador.ToLower() == nome_colaborador.ToLower())) return false;
-    return true;
-  }
-  private Int32 if_exist(String? funcionario)
-  {
-    if(funcionario == null) return 0;
-    var func = this.database.funcionario.Where(o => o.nome_colaborador.ToLower() == funcionario.ToLower()).SingleOrDefault();
-    if(func == null) return 0;
-    return func.matricula;
-  }
-  private enum ExpectedType {Text, Number, Date, Time, Enum, Placa}
-  private String abreviacao(String recurso)
-  {
-    var re1 = new System.Text.RegularExpressions.Regex("^([A-Z]{3,})");
-    var re2 = new System.Text.RegularExpressions.Regex("([A-Z][a-z]{1,})");
-    var re3 = new System.Text.RegularExpressions.Regex("([0-9]{3})");
-    if(!re3.IsMatch(recurso)) return String.Empty;
-    var abreviado = "";
-    recurso = recurso.Replace('–', '-');
-    abreviado += re1.Match(recurso).Value;
-    if(re2.Match(recurso).Value == "Religa") abreviado += 'R';
-    if(re2.Match(recurso).Value == "Corte") abreviado += 'C';
-    abreviado += re3.Match(recurso).Value;
-    return abreviado;
   }
   private Dictionary<String, Int32> Cabecalho(List<String> primeira_linha)
   {
