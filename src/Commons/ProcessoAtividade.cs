6663bdc720ee69200a3bc400323ecc83f79ff579
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2024-07-23 09:59:14 -0300

    Removed attribute to define primary key in model class

--- src/Commons/ProcessoAtividade.cs
+++ src/Commons/ProcessoAtividade.cs
@@ -5,8 +5,6 @@ namespace backend.Commons
 {
   public class ProcessoAtividade
   {
-    [Key]
-    [DatabaseGenerated(DatabaseGeneratedOption.None)]
     public Int32 id_atividade { get; set; }
     public Int32 id_processo { get; set; }
     public ProcessoAtividade(Int32 id_atividade, Int32 id_processo)
